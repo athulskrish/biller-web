79ea794cc6f09f464c14d262241eadc76d8675e8
athul <athulsklearns@gmail.com>
2024-11-09 21:52:47 +0530

    changed db_connect username and password

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
  define('HOST','localhost');         //hostname
- define('USER','keramfvp_admin');     //username
- define('PASS','AMAL@2024');        //user password
+ define('USER','root');     //username keramfvp_admin
+ define('PASS','');        //user password AMAL@2024
  define('DB','keramfvp_smartpos');  //database name
  
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
